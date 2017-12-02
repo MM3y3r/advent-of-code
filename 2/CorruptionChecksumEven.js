59d7dff9543c8f1de0e3c31a81638981396a6634
Maximilian Meyer <maximilian.meyer@br.de>
2017-12-02 23:49:19 +0100

    EVEN 💥

---
+++ 2/CorruptionChecksumEven.js
@@ -0,0 +1,23 @@
+if(typeof require !== 'undefined') XLSX = require('xlsx');
+const workbook = XLSX.readFile('spreadsheet_v2.xlsx');
+const myData = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {header:1});
+
+const corruptionChecksum = (data) => {
+	let finalSum = 0;
+
+	// my LOOP
+	for(let i = 0; i < data.length; i++){
+		for(let j = 0; j < data[i].length; j++){
+			const current = parseInt(data[i][j]);
+			for(let u = 0; u < data[i].length; u++){
+				const temp = parseInt(data[i][u]);
+				if(current % temp === 0 && current !== temp){
+					finalSum = finalSum + (current/temp);
+				}
+			}
+		}
+	}
+	return finalSum;
+}
+
+console.log(corruptionChecksum(myData))
