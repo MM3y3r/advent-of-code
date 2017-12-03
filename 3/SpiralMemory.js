732e01b96c3e47bde782c661d0f14145aa5558de
Maximilian Meyer <maximilian.meyer@br.de>
2017-12-03 15:03:24 +0100

    struct

---
+++ 3/SpiralMemory.js
@@ -0,0 +1,12 @@
+type mapField = {
+	distanceToCenter:Function,
+	value: number,
+	south: mapField,
+	north: mapField,
+	east: mapField,
+	west: mapField,
+}
+
+const spiralMemory = (number) => {
+
+}
